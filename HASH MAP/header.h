48f709b9cb4eae1000a18dae99f4464c2f1724b7
Wanderson-Patricio <118014817+Wanderson-Patricio@users.noreply.github.com>
2023-09-13 13:00:30 -0300

    Update header.h

--- HASH MAP/header.h
+++ HASH MAP/header.h
@@ -1,9 +1,9 @@
 #ifndef HASH_H_INCLUDED
 #define HASH_H_INCLUDED
 
-#define MAX_RESOURCES 3
-#define MAX_PERMUTATIONS 6 //4 recursos por compartimento, dando 4! permutacções
-#define MAX_SIZE 556 //5 diferentes recursos (555+1)
+#define MAX_RESOURCES 3 // A quantidade de recursos por compartimento
+#define MAX_PERMUTATIONS 6 // 3 recursos por compartimento, dando 3! permutacÃ§Ãµes
+#define MAX_SIZE 556 // 5 diferentes recursos (555+1)
 
 typedef struct resource{
     char nome[50];
@@ -29,24 +29,31 @@ Compartment createCompartment(int ids[]);
 
 void infoCompartment(Compartment c);
 
+int isEquals(Compartment a, Compartment b);
+
 Table* createTable();
 
 void deleteTable(Table* t);
 
+void infoTable(Table* t);
+
 /************************************************************/
 
-// Função para concatenar 4 inteiros
+// FunÃ§Ã£o para concatenar 4 inteiros
 int concatenateIntegers(int numbers[], int n);
 
 void selectionSort(int arr[], int n);
 
-// Função hash para o compartimento
+// FunÃ§Ã£o hash para o compartimento
 int h(Compartment c);
 
 int verificaAbertura(Table* t, int linha);
 
 void resetar(Table* t, int linha);
 
+int isNewPermutation(Table* t, int linha, Compartment c);
+
 void insertCompartment(Table* t, Compartment c);
+
 
 #endif // HASH_H_INCLUDED
