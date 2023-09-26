f86ea2b04d1f0bc72e811f409a4a641ccac50dbb
Anderson <115733205+andere2015@users.noreply.github.com>
2023-09-26 16:06:39 -0300

    Update header.h

--- HASH MAP/header.h
+++ HASH MAP/header.h
@@ -2,18 +2,21 @@
 #define HASH_H_INCLUDED
 
 #define MAX_RESOURCES 3 // A quantidade de recursos por compartimento
-#define MAX_PERMUTATIONS 6 // 3 recursos por compartimento, dando 3! permutacções
-#define MAX_SIZE 556 // 5 diferentes recursos (555+1)
+#define MAX_PERMUTATIONS 6 // 3 recursos por compartimento, dando 3! permutacÃ§Ãµes
+#define MAX_SIZE 667 // 6 diferentes recursos (666+1)
 
+// Anderson
 typedef struct resource{
     char nome[50];
     int id;
 }Resource;
 
+// Anderson
 typedef struct compartment{
     Resource recursos[MAX_RESOURCES];
 }Compartment;
 
+// Anderson
 typedef struct table{
     Compartment tabela[MAX_SIZE][MAX_PERMUTATIONS];
     int qtds[MAX_SIZE];
@@ -29,6 +32,7 @@ Compartment createCompartment(int ids[]);
 
 void infoCompartment(Compartment c);
 
+// Anderson
 int isEquals(Compartment a, Compartment b);
 
 Table* createTable();
@@ -39,18 +43,24 @@ void infoTable(Table* t);
 
 /************************************************************/
 
-// Função para concatenar 4 inteiros
+// FunÃ§Ã£o para concatenar 4 inteiros
+// Anderson
 int concatenateIntegers(int numbers[], int n);
 
+// Anderson (Cria HEAP Sort e muda)
 void selectionSort(int arr[], int n);
 
-// Função hash para o compartimento
+// FunÃ§Ã£o hash para o compartimento
+// Anderson
 int h(Compartment c);
 
+// Anderson
 int verificaAbertura(Table* t, int linha);
 
+// Anderson
 void resetar(Table* t, int linha);
 
+// Anderson
 int isNewPermutation(Table* t, int linha, Compartment c);
 
 void insertCompartment(Table* t, Compartment c);
