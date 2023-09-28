7637e270a5d392760f0a8a78729cb32ed2b5589f
Anderson <115733205+andere2015@users.noreply.github.com>
2023-09-28 11:55:34 -0300

    Update main.c

--- HASH MAP/main.c
+++ HASH MAP/main.c
@@ -4,47 +4,33 @@
 #include <locale.h>
 #include "header.h"
 
+#define MAX_LINHAS 1024
+
 int main(){
     setlocale(LC_ALL, "portuguese");
-    int ids1[]  = {3,1,2};
-    int ids2[]  = {1,3,2};
-    int ids3[]  = {2,1,3};
-    int ids4[]  = {2,3,1};
-    int ids5[]  = {5,2,4};
-    int ids6[]  = {3,2,1};
-    int ids7[]  = {2,4,5};
-    int ids8[]  = {2,5,4};
-    int ids9[]  = {4,2,5};
-    int ids10[] = {5,4,2};
-    int ids11[] = {1,2,3};
-    Compartment c1  = createCompartment(ids1);
-    Compartment c2  = createCompartment(ids2);
-    Compartment c3  = createCompartment(ids3);
-    Compartment c4  = createCompartment(ids4);
-    Compartment c5  = createCompartment(ids5);
-    Compartment c6  = createCompartment(ids6);
-    Compartment c7  = createCompartment(ids7);
-    Compartment c8  = createCompartment(ids8);
-    Compartment c9  = createCompartment(ids9);
-    Compartment c10 = createCompartment(ids10);
-    Compartment c11 = createCompartment(ids11);
 
+    Table *t = createTable();
 
-    Table* t = createTable();
+    int ids[MAX_RESOURCES], coluna;
+    char *S[MAX_LINHAS], *ptr;
+    FILE *fptr;
 
-    for(int i=0; i<100; i++){
-        insertCompartment(t, c1);
-        insertCompartment(t, c2);
-        insertCompartment(t, c3);
-        insertCompartment(t, c4);
-        insertCompartment(t, c5);
-        insertCompartment(t, c6);
-        insertCompartment(t, c7);
-        insertCompartment(t, c8);
-        insertCompartment(t, c9);
-        insertCompartment(t, c10);
-        insertCompartment(t, c11);
+    fptr = fopen("recursos1 - Copia.csv", "r");
+
+    while(fgets(S, MAX_LINHAS, fptr) != NULL){
+        ptr = strtok(S, ",");
+        coluna = 0;
+
+        while(ptr != NULL){
+            //ids[coluna] = atoi(ptr);
+
+            printf("%s ", ptr);
+            coluna++;
+            ptr = strtok(NULL, ",");
+        }
     }
+
+    fclose(fptr);
 
     printf("\n");
     infoTable(t);
